3f040bf46d9f985668cbb8fc5c8f54f859fac64d
Mouster Chung <131771942+mousterchung@users.noreply.github.com>
2025-04-13 06:17:32 +0800

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,6 @@
+var frame_top = document.getElementById('frame_top'); 
+var frame_left = document.getElementById('frame_left'); 
+var frame_right = document.getElementById('frame_right'); 
+var window_dict = {top: frame_top, 
+				   left: frame_left, 
+				   right: frame_right}; 
